7499fdff38f52c0cc88ce0e5c4d25a4ee6c44980
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2017-08-01 21:29:17 -0400

    Fixed code

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
 var weatherApp = {
-	userLat: 0,
-	userLong: 0,
 	request: null,
-	requestURL: "",
-	city: null,
-	temp: 0,
-	weather: null,
 	getLocation: function() {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(pos) {
-				userLat = pos.coords.latitude;
-				userLong = pos.coords.longitude;
+				let userLat = pos.coords.latitude;
+				let userLong = pos.coords.longitude;
+				request = new XMLHttpRequest();
+				let requestURL = "https://fcc-weather-api.glitch.me/api/current?lon=" + userLong + "&lat=" + userLat;
+				request.open('GET', requestURL);
+				request.responseType = 'json';
+				request.send();
 				weatherApp.apiRequest();
 			});
 		}
 	},
-	apiRequest: function() {
-		request = new XMLHttpRequest();
-		requestURL = "https://fcc-weather-api.glitch.me/api/current?lon=" + userLong + "&lat=" + userLat;
-		request.open('GET', requestURL);
-		request.responseType = 'json';
-		request.send();
-		request.onload = function(res) {
+	apiRequest: function() {		
+		request.onload = function() {
 			console.log(request.response);
-			city = request.response.name;
-			temp = request.response.main.temp;
-			weather = request.response.weather[0].main;
-			icon = request.response.weather[0].icon;
-		}
-		setTimeout(function() {
+			let city = request.response.name;
+			let temp = request.response.main.temp;
+			let weather = request.response.weather[0].main;
+			let icon = request.response.weather[0].icon;
 			$("#loading").remove();
 			$("#weatherBody").append("<h2 id='cityName'>" + city + "</h2>");
 			$("#cityName").append("<h3 id='temperature'>" + (Math.round(temp * 9 / 5 + 32)) + "&deg;F</h3>");
 			$("#temperature").append("<h3 id='weatherStatus'>" + weather + "</h3>");
 			$("#weatherStatus").append("<img src='" + icon + "'>");	
-		}, 1000)		
+		}	
 	},
 }
 
